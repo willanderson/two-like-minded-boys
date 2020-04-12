14bfacc771a0331c08db1d024d2e4cd65ba89568
James La Marre <jameslamarre@gmail.com>
2020-04-12 14:20:44 -0400

    added new listeners

--- js/app.js
+++ js/app.js
@@ -5,6 +5,9 @@ $(document).ready(function() {
   var vid2 = document.getElementById('seth');
   var videos = [vid1, vid2]
 
+  var leftArea = document.querySelector('.left-area');
+  var rightArea = document.querySelector('.right-area');
+
   var cube = document.querySelector('.cube');
   var scene = document.querySelector('.scene');
   var radioGroup = document.querySelector('.radio-group');
@@ -20,12 +23,15 @@ $(document).ready(function() {
     }
     cube.classList.add(showClass);
     currentClass = showClass;
-
   }
+
   // set initial side
   changeSide();
 
   radioGroup.addEventListener('change', changeSide);
+
+  leftArea.addEventListener('change', changeSide);
+  rightArea.addEventListener('change', changeSide);
 
 
   $(".button").on('click',
